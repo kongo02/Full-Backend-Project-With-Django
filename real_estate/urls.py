0855d7327f1969d443c2c80fe0c57d2b5e5bc25b
kongo02 <bokangakogo02@gmail.com>
2024-04-18 11:38:34 +0200

    Update user profile

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path("supersecret/", admin.site.urls),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("api/v1/profile/", include("apps.profiles.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Real Estate Admin"
 admin.site.site_title = "Real Estate Admin Portal"
 admin.site.index_title = "Welcome to the Real Estate Portal"
-
+#print("2:14")
